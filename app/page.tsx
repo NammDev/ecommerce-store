87f6706afc16b916f253daa5eb36a715751a8ffb
Nam <namdeveloper.ca@gmail.com>
2023-10-28 14:45:25 +0700

    show billboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
+import getBillboards from '@/actions/get-billboard'
 import Billboard from '@/components/Billboard'
 import Container from '@/components/ui/container'
 
-export default function HomePage() {
+export const revalidate = 0
+
+export default async function HomePage() {
+  const billboard = await getBillboards('9c523497-e15f-4920-8676-36fa4057c192')
+
   return (
     <Container>
       <div className='space-y-10 pb-10'>
-        <Billboard />
+        <Billboard data={billboard} />
       </div>
     </Container>
   )
