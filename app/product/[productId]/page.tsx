d4fc06226c5b6d22510db08d17810c68b3b97c78
Nam <namdeveloper.ca@gmail.com>
2023-10-29 20:59:17 +0700

    List Images of Product by @headless/tab

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,8 +1,8 @@
 import getProduct from '@/actions/get-product'
 import getProducts from '@/actions/get-products'
 import ProductList from '@/components/ProductList'
+import Gallery from '@/components/gallery'
 import Container from '@/components/ui/container'
-import ProductCard from '@/components/ui/product-card'
 
 const ProductPage = async ({ params }: { params: { productId: string } }) => {
   const product = await getProduct(params.productId)
@@ -15,7 +15,7 @@ const ProductPage = async ({ params }: { params: { productId: string } }) => {
         <div className='px-4 py-10 sm:px-6 lg:px-8'>
           <div className='lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8'>
             {/* Galerry */}
-            <div>Galerry</div>
+            <Gallery images={product.images} />
             <div className='mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0'>
               {/* Info */}
               Info
