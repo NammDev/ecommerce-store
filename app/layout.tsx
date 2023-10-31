f09dc42614649b45cdcd7e64772c3a0f87f833fb
Nam <namdeveloper.ca@gmail.com>
2023-10-31 16:38:28 +0700

    add react hot toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
 import ModalProvider from '@/providers/modal-provider'
+import { Toaster } from 'react-hot-toast'
 
 const font = Urbanist({ subsets: ['latin'] })
 
@@ -16,7 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='en'>
       <body className={font.className}>
-        {' '}
+        <Toaster />
         <ModalProvider />
         <Navbar />
         {children}
